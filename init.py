aae5698afaf7099ed684655c5d444a83f6504855
Matias <Matias-DR@outlook.com>
2020-06-03 03:22:36 -0300

    Normas PEP8 aplicadas

--- init.py
+++ init.py
@@ -1,33 +1,32 @@
 from PySimpleGUI import *
 from items import *
 
+
 def menu():
-	ventana=Window(
-									'Visor', elementos('menu'),
-									margins=(1, 1),
-									resizable=True,
-									text_justification='center',
-									background_color='#242424',
-									finalize=True
-								)
+    ventana = Window('Visor', elementos('menu'),
+                     margins=(1, 1),
+                     resizable=True,
+                     text_justification='center',
+                     background_color='#242424',
+                     finalize=True)
 
-#def visor():
-#	ventana=Window(
-#									'Visor', elementos('visor'),
-#									size=(253, 473),
-#									margins=(1, 1),
-#									location=(380, 30),
-#									resizable=True,
-#									text_justification='center',
-#									background_color='#242424',
-#									#return_keyboard_events=True,
-#									finalize=True
-#								)
+# def visor():
+#  ventana=Window('Visor', elementos('visor'),
+#                 size=(253, 473),
+#                 margins=(1, 1),
+#                 location=(380, 30),
+#                 resizable=True,
+#                 text_justification='center',
+#                 background_color='#242424',
+#                 return_keyboard_events=True,
+#                 finalize=True
+#                )
 
-	while True:
-		evento, valor=ventana.read()
-		if evento in (None, 'Salir'):
-			break
-	ventana.close()
+    while True:
+        evento, valor = ventana.read()
+        if evento in (None, 'Salir'):
+            break
+    ventana.close()
+
 
 menu()
--- items.py
+++ items.py
@@ -1,30 +1,21 @@
 from PySimpleGUI import *
 from imagenes import *
 
+
 def elementos(tipo):
-	if tipo=='menu':
-		return	[
-					[
-						Image(
-								'media/carta/sobre.png',
-								pad=(0, 0))
-					], 
-					[
-						Button(
-								'Ver fotos',
-								size=(15, 0),
-								pad=(2, 2),
-								border_width=0,
-								button_color=('#292929', '#292929'),
-								image_filename=botonRandom(),
-								image_size=(120, 120)),
-						Button(
-								'Salir',
-								size=(15, 0),
-								pad=(2, 2),
-								border_width=0,
-								button_color=('#292929', '#292929'),
-								image_filename=botonRandom(),
-								image_size=(120, 120))
-					]
-				]
+    if tipo == 'menu':
+        return [[Image('media/carta/sobre.png', pad=(0, 0))],
+                [Button('Ver fotos',
+                        size=(15, 0),
+                        pad=(2, 2),
+                        border_width=0,
+                        button_color=('#292929', '#292929'),
+                        image_filename=botonRandom(),
+                        image_size=(120, 120)),
+                 Button('Salir',
+                        size=(15, 0),
+                        pad=(2, 2),
+                        border_width=0,
+                        button_color=('#292929', '#292929'),
+                        image_filename=botonRandom(),
+                        image_size=(120, 120))]]
